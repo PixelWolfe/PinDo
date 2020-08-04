c5ae6e9bb6ade174936e70648c780103ca9215c5
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-03 21:39:31 -0500

    Added base tables to database.sql, created route templates

--- database.sql
+++ database.sql
@@ -4,7 +4,51 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username VARCHAR (80) UNIQUE NOT NULL,
+    password VARCHAR (1000) NOT NULL,
+    email_address VARCHAR (150) NOT NULL
+);
+
+CREATE TABLE "project" (
+	id SERIAL PRIMARY KEY,
+	user_id int,
+	image_url varchar(15000),
+	description varchar(250)
+);
+
+CREATE TABLE "list" (
+	id SERIAL PRIMARY KEY,
+	title varchar(100),
+	board_id int,
+	color_id int
+);
+
+CREATE TABLE "note" (
+	id SERIAL PRIMARY KEY,
+	title varchar(100),
+	text varchar(10000),
+	board_id int,
+	color_id int
+);
+
+CREATE TABLE "image" (
+	id SERIAL PRIMARY KEY,
+	title varchar(100),
+	url varchar(15000),
+	board_id int,
+	color_id int
+);
+
+CREATE TABLE "task" (
+	id SERIAL PRIMARY KEY,
+	list_id int,
+	position int,
+	description varchar(250),
+	completed boolean
+);
+
+CREATE TABLE "color" (
+	id SERIAL PRIMARY KEY,
+	color varchar(50)
 );
