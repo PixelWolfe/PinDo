2e933a77e7a88510e77fec51950fda7abf8da024
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-04 14:32:46 -0500

    Made rough Login page format/Component Template

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -4,6 +4,14 @@ import { connect } from 'react-redux';
 import {Grid, Button, TextField} from '@material-ui/core';
 import Fade from 'react-reveal/Fade'
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
 class LoginPage extends Component {
   state = {
     username: '',
@@ -35,54 +43,76 @@ class LoginPage extends Component {
   render() {
     return (
       <Fade>
-        <Grid container>
-          <Grid item xs={12} align='center'>
-            <div style={{backgroundColor: 'cyan'}}>
-            {this.props.errors.loginMessage && (
-              <h2
-                className="alert"
-                role="alert"
-              >
-                {this.props.errors.loginMessage}
-              </h2>
-            )}
-            <form onSubmit={this.login}>
-              <h1>Login</h1>
-              <div>
-    
-                <TextField
-                  variant="filled"
-                  label="username"
-                  inputProps={{maxLength: 80}}
-                  value={this.state.username}
-                  onChange={this.handleInputChangeFor('username')}
-                />
-              </div>
-              <div>
-      
+
+ 
+            <div style={{backgroundColor: 'lightgreen'}}>
+              <Grid container justify='center' alignItems='center' alignContent='center'>
+              
+              <Grid xs={6} item align='center'>
+                  <div>
+                  {
+                    this.props.errors.loginMessage && 
+                    (<h2 className="alert" role="alert">{this.props.errors.loginMessage}</h2>)
+                  }
+                  <h1>Login</h1>
+  
+                  <TextField
+                    variant="filled"
+                    label="username"
+                    inputProps={{maxLength: 80}}
+                    value={this.state.username}
+                    onChange={this.handleInputChangeFor('username')}
+                  />
+                  <br/>
                   <TextField
                     variant="filled"
                     label="password"
+                    type='password'
                     inputProps={{maxLength: 1000}}
                     value={this.state.password}
                     onChange={this.handleInputChangeFor('password')}
                   />
-        
+                  <br/>
+                  <Button 
+                    name="submit" 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={this.login}
+                  >
+                      Log In
+                  </Button>
+              
+                    <br/>
+                    <br/>
+                  <Button 
+                    alignContent='right' 
+                    size="small" 
+                    variant="outlined" 
+                    color="primary" 
+                    onClick={() => {this.props.dispatch({type: 'SET_TO_REGISTER_MODE'})}}
+                  >
+                    Register
+                  </Button>
               </div>
-              <div>
-                <Button type="submit" name="submit" variant="contained" color="primary">Log In</Button>
-              </div>
-              <br/>
-              <br/>
-            </form>
-            
-            <Button alignContent='right' size="small" variant="outlined" color="primary" onClick={() => {this.props.dispatch({type: 'SET_TO_REGISTER_MODE'})}}>Register</Button>
+                
+              </Grid>
 
-          </div>
-          </Grid>
-        </Grid>
-      </Fade>
+              <Grid xs={6} item align='center'>
+                  <div>
+                    <img 
+                      style={{
+                        height: "500px",
+                      }} 
+                      src='https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg' 
+                      alt="flower picture"
+                    />
+                  </div>
+              </Grid>
+            
+              </Grid>
+            </div>
     
+      </Fade>
     );
   }
 }
