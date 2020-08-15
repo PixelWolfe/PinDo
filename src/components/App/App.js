fdaea7cefa33bde2c3384cf568724493f472387b
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-15 15:24:30 -0500

    removed unused import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,6 @@ import {
 import {connect} from 'react-redux';
 
 import NavHome from '../NavHome/NavHome';
-import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
